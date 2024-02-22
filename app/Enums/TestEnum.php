64f691788c561c9d70651922f20132204f142b64
shang.hsu <shang.hsu@homeplus.net.tw>
2024-02-22 09:49:45 +0800

    feat: Example of a new architecture.

---
+++ app/Enums/TestEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+enum TestEnum: string
+{
+    // case Text = 'text';
+    // case Textarea = 'textarea';
+    // case Select = 'select';
+    // case Radio = 'radio';
+    // case Checkbox = 'checkbox';
+}
